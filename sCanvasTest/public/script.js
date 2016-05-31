e74857148c03aebbf7109e1218d231168855ba4c
Jason Chan <jason.chan@techflex.com>
2016-05-31 09:01:54 -0400

    moving img spawn

--- sCanvasTest/public/script.js
+++ sCanvasTest/public/script.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
 	var canvas = new fabric.Canvas('c');
+	canvas.setDimensions({ width: 600, height: 600 });
 
-var imgElement = document.getElementById('my-image');
-var imgInstance = new fabric.Image(imgElement, {
-  left: 100,
-  top: 100,
-  angle: 90,
-  opacity: 0.85
-});
-canvas.add(imgInstance);
+// Controls
+	// text
+	$("#sendtxt").click(function(){
+		var msg = document.getElementById("text").value;
+		var text = new fabric.Text(msg, { left: 100, top: 100 });
+	    // Clear the box
+	    document.getElementById("text").value = "";
+	    canvas.add(text);
+	  })
 
-var text = new fabric.Text('hello world', { left: 100, top: 100 });
-canvas.add(text);
+	// pic link
+	$("#sendimg").click(function(){
+	    var pic = document.getElementById("img").value;
+	    fabric.Image.fromURL(pic, function(oImg) {
+		    // Set the image so its top left corner aligns with the matching canvas edge 
+			oImg.left=(oImg.width)/2;
+			oImg.top=(oImg.height)/2;
+			// Clear the box
+			document.getElementById("img").value = "";
+			canvas.add(oImg);
+		});
+	})
+	
 });
